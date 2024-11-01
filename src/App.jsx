751bec627a5a9b8f6dded86b487ff0c7910ee6eb
giout <giovanniurdanetatulli@gmail.com>
2024-11-01 18:03:48 -0400

    feat: footer
    - add footer
    - solve issue with active button

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,23 @@ function App() {
   return (
     <>
       <Router>
-        <header>
-          <Navbar />
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Navigate to="/name"/>} />
-            <Route path="/name" element={<Fetch section="Name"/>}/>
-            <Route path="/author" element={<Fetch section="Author"/>}/>
-            <Route path="/subject" element={<Fetch section="Subject"/>}/>
-          </Routes>
-        </main>
+        <div className="page-container">
+          <header>
+            <Navbar />
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<Navigate to="/name"/>} />
+              <Route path="/name" element={<Fetch section="Name"/>}/>
+              <Route path="/author" element={<Fetch section="Author"/>}/>
+              <Route path="/subject" element={<Fetch section="Subject"/>}/>
+            </Routes>
+          </main>
+          <footer>
+            <span>Giovanni Urdaneta - 2024</span>
+            <span>Data provided by <strong><a href="https://openlibrary.org/developers/api" target="_blank">OpenLibrary API</a></strong></span>
+          </footer>
+        </div>
       </Router>
     </>
   )
