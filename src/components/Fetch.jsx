af855ff670c5b8f20a733da247c8bdffb688d1b4
giout <giovanniurdanetatulli@gmail.com>
2024-11-01 20:22:26 -0400

    feat: add sample data

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -7,6 +7,51 @@ import BookCard from './BookCard';
 function Fetch(props) {
   const { setSection } = useSection();
 
+  const books = [
+    {
+      cover: "https://covers.openlibrary.org/b/isbn/9781611748864-L.jpg",
+      name: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      date: 1954,
+      rating: 4.556962,
+      subjects: [
+        "The Lord of the Rings",
+        "Fiction",
+        "Ficción",
+        "English Fantasy fiction",
+        "Ficción fantástica inglesa",
+      ],
+      characters: [
+        "Frodo Baggins",
+        "Samwise Gamgee",
+        "Meriadoc Brandybuck",
+        "Peregrin Took",
+        "Gandalf the Grey",
+      ]
+    },
+    {
+      cover: "https://covers.openlibrary.org/b/isbn/0340951419-L.jpg",
+      name: "Fishing Log",
+      author: "Harold RIVERA",
+      date: 2022,
+      rating: 4.556962,
+      subjects: [
+        "The Lord of the Rings",
+        "Fiction",
+        "Ficción",
+        "English Fantasy fiction",
+        "Ficción fantástica inglesa",
+      ],
+      characters: [
+        "Frodo Baggins",
+        "Samwise Gamgee",
+        "Meriadoc Brandybuck",
+        "Peregrin Took",
+        "Gandalf the Grey",
+      ]
+    },
+  ]
+
   useEffect(() => {
     setSection(props.section);
   }, [props.section, setSection])
@@ -15,7 +60,15 @@ function Fetch(props) {
     <div className="fetch-container">
       <SearchBar />
       <div className="cards-container">
-        <BookCard />
+        {
+          books.map((book, index) => (
+            <BookCard 
+              key={index}
+              title={book.name}
+              date={book.date}
+              imgSrc={book.cover} />
+          ))
+        }
       </div>
     </div>
   )
