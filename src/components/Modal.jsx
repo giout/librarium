c8cbc135dd90c84d76c92278d8d0bf95e30b8a63
giout <giovanniurdanetatulli@gmail.com>
2024-11-05 18:55:58 -0400

    feat: change main icon

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -17,8 +17,8 @@ function Modal(props) {
                         <span className="modal-title modal-wrap-text">{props.title}</span>
                         <hr className="modal-line"/>
                         <div className="modal-icon-text-container">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-film modal-icon first-color-icon" viewBox="0 0 16 16">
-                                <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-calendar modal-icon first-color-icon" viewBox="0 0 16 16">
+                                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
                             </svg>
                             <span className="modal-text">Release date: {props.date}</span>
                         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,8 +33,10 @@ function Navbar() {
   return (
     <>
         <nav className="navbar1">
-            <img className="book-icon" src="./public/book.svg" alt="" />
-            <span className="white-text title">Memorabilia</span>
+          <svg xmlns="http://www.w3.org/2000/svg" className="bi bi-film film-icon" viewBox="0 0 16 16">
+            <path d="M0 1a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1zm4 0v6h8V1zm8 8H4v6h8zM1 1v2h2V1zm2 3H1v2h2zM1 7v2h2V7zm2 3H1v2h2zm-2 3v2h2v-2zM15 1h-2v2h2zm-2 3v2h2V4zm2 3h-2v2h2zm-2 3v2h2v-2zm2 3h-2v2h2z"/>
+          </svg>
+          <span className="white-text title">Memorabilia</span>
         </nav>
         <nav className="navbar2">
             <button 
