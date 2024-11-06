d790c841694e8a43007c66d7e9ac02c134d5b378
giout <giovanniurdanetatulli@gmail.com>
2024-11-06 00:58:32 -0400

    feat: handle pagination with useAPI hook

--- src/store/useAPI.js
+++ src/store/useAPI.js
@@ -1,16 +1,18 @@
 import { create } from 'zustand';
 import { searchForMedia } from '../api/get/media';
 
-const useAPI = create((set) => ({
+const useAPI = create((set, get) => ({
     data: [],
     results: 0,
-    clean: () => set({ data: [] }),
-    fetchData: async (section, param, page) => {
-        const result = await searchForMedia(section, param, page);
-        set ({
-            data: result.media,
-            results: result.results
-        })
+    page: 0,
+    clean: () => set({ data: [], results: 0, page: 0 }),
+    fetchData: async (section, param) => {
+        const result = await searchForMedia(section, param, get().page + 1);
+        set({
+            data: [...get().data, ...result.media],
+            results: result.results,
+            page: get().page + 1
+        });
     }
 }));
 
