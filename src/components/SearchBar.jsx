5577fe0f0e81aa38882c6ab359754fde425ecd4e
giout <giovanniurdanetatulli@gmail.com>
2024-11-07 13:20:52 -0400

    feat: clean input button

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,7 +4,7 @@ import useSearchInput from '../store/useSearchInput';
 
 function SearchBar() {
     const { section } = useSection();
-    const { setSearchInput } = useSearchInput();
+    const { searchInput, setSearchInput } = useSearchInput();
 
     const handleInput = (e) => {
         setSearchInput(e.target.value);
@@ -12,13 +12,22 @@ function SearchBar() {
 
     return (
     <div className="search-bar">
-        <input className="search-input" type="text" placeholder={`Search for ${section}...`} onChange={(e) => handleInput(e)}/>
+        <input className="search-input" type="text" placeholder={`Search for ${section}...`} onChange={(e) => handleInput(e)} value={searchInput}/>
         <button 
             className="search-button" 
-            disabled={true}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-search search-icon" viewBox="0 0 16 16">
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-        </svg>
+            onClick={() => setSearchInput('')}
+            disabled={!searchInput}>
+            {
+                searchInput ? (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-x cancel-icon" viewBox="0 0 16 16">
+                        <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
+                    </svg>
+                ) : (
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-search search-icon" viewBox="0 0 16 16">
+                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+                    </svg>
+                )
+            }
         </button>
     </div>
     )
